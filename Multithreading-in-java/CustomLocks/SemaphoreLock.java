e1231f4948b77ddfc6572f99168a1fe12e5b1791
TanveerSingh31 <tanveersinghnayyar@gmail.com>
2025-04-27 15:48:32 +0530

    Semaphore lock

--- Multithreading-in-java/CustomLocks/SemaphoreLock.java
+++ Multithreading-in-java/CustomLocks/SemaphoreLock.java
@@ -1,4 +1,64 @@
 package CustomLocks;
 
+
+import java.util.concurrent.Semaphore;
+
+class Sample4 {
+
+    // Maximum threads that can apply lock on the resource
+    // Only 2 threads will be allowed at a time to acquire lock
+    Semaphore semaphoreLockObj = new Semaphore(2);
+
+    public void getData(){
+
+        try{
+            semaphoreLockObj.acquire();
+            System.out.println("lock acquired by " + Thread.currentThread().getName() );
+            Thread.sleep(4000);
+            semaphoreLockObj.release();
+            System.out.println("lock released by " + Thread.currentThread().getName() );
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+}
+
+
 public class SemaphoreLock {
+
+
+    public static void main(String [] args) {
+
+        Sample4 sampleObj = new Sample4();
+
+        Thread t1 = new Thread(()->{
+            sampleObj.getData();
+        });
+
+        Thread t2 = new Thread(()->{
+            sampleObj.getData();
+        });
+        Thread t3 = new Thread(()->{
+            sampleObj.getData();
+        });
+        Thread t4 = new Thread(()->{
+            sampleObj.getData();
+        });
+
+
+        // Only 2 threads will be allowed at a time to acquire lock
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+
+    }
+
+
 }
--- Multithreading-in-java/CustomLocks/StampedLockExample.java
+++ Multithreading-in-java/CustomLocks/StampedLockExample.java
@@ -1,6 +1,84 @@
 package CustomLocks;
 
+import java.util.concurrent.locks.StampedLock;
+
 public class StampedLockExample {
 
+    int a = 100;
+    StampedLock stampedLockObj = new StampedLock();
+
+
+    public void readData() {
+        System.out.println("readData invoked");
+        long stamp = stampedLockObj.tryOptimisticRead();
+
+        try{
+            Thread.sleep(5000);
+        }catch(Exception e){}
+
+        if(stampedLockObj.validate(stamp)){
+            a = 200;
+            System.out.println("updated the resource");
+        }
+        else{
+            System.out.println("Rollback, as resource updated by another thread");
+        }
+
+    }
+
+
+
+    public void writeData(int data) {
+        System.out.println("Write Data invoked");
+        long stamp = stampedLockObj.writeLock();
+        try{
+            Thread.sleep(2000);
+        }catch(Exception e){}
+
+        this.a = data;
+        stampedLockObj.unlockWrite(stamp);
+    }
+
+
+
+
+
+    public static void main(String [] args){
+
+        // Stamped lock contains all method of the read-write lock
+
+        // optimistic locking
+        // If while the thread is doing optimistic read -> some other thread has updated the resource
+        // When Optimistic lock will validate before updating -> it will return false
+        // Hence we have to rollback
+
+        StampedLockExample obj = new StampedLockExample();
+
+        Thread t1 = new Thread(()->{
+            obj.readData();
+        });
+
+        Thread t2 = new Thread(()->{
+            obj.writeData(1000);
+        });
+
+
+        t1.start();
+        t2.start();
+
+        // waiting for all threads to finish its work
+        try{
+            t1.join();
+            t2.join();
+        }
+        catch(Exception e) {
+
+        }
+
+
+        System.out.println(obj.a);
+
+
+    }
 
 }
